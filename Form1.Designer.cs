53a3334237616c55099a06ed30683ff357d13cc9
Misha <misha.gefkm14@gmail.com>
2024-02-20 23:12:17 +0200

    Game

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace game2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.bot1 = new System.Windows.Forms.PictureBox();
+            this.bot2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.Color.Transparent;
+            this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+            this.pic1.Location = new System.Drawing.Point(0, 0);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(629, 455);
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(177, 279);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 89);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackColor = System.Drawing.Color.Transparent;
+            this.pic2.Image = ((System.Drawing.Image)(resources.GetObject("pic2.Image")));
+            this.pic2.Location = new System.Drawing.Point(0, -475);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(629, 475);
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick_1);
+            // 
+            // bot1
+            // 
+            this.bot1.BackColor = System.Drawing.Color.Transparent;
+            this.bot1.Image = ((System.Drawing.Image)(resources.GetObject("bot1.Image")));
+            this.bot1.Location = new System.Drawing.Point(100, -100);
+            this.bot1.Name = "bot1";
+            this.bot1.Size = new System.Drawing.Size(71, 89);
+            this.bot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bot1.TabIndex = 3;
+            this.bot1.TabStop = false;
+            // 
+            // bot2
+            // 
+            this.bot2.BackColor = System.Drawing.Color.Transparent;
+            this.bot2.Image = ((System.Drawing.Image)(resources.GetObject("bot2.Image")));
+            this.bot2.Location = new System.Drawing.Point(253, -130);
+            this.bot2.Name = "bot2";
+            this.bot2.Size = new System.Drawing.Size(71, 89);
+            this.bot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bot2.TabIndex = 4;
+            this.bot2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(184, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 44);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Вы проиграли";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(255, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 44);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Играть";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // coin
+            // 
+            this.coin.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.coin.Image = ((System.Drawing.Image)(resources.GetObject("coin.Image")));
+            this.coin.Location = new System.Drawing.Point(339, -400);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(55, 50);
+            this.coin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin.TabIndex = 8;
+            this.coin.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Монеты 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(11, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 27);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Собранные монеты ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(629, 455);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.bot2);
+            this.Controls.Add(this.bot1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.pic2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox bot1;
+        private System.Windows.Forms.PictureBox bot2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace game2
+{
+    public partial class Form1 : Form
+    {
+        private Point pos;
+        private bool dragging,lose=false;
+        private int countCoins = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pic1.MouseDown += MouseClickDown;
+            pic1.MouseUp += MouseClickUp;
+            pic1.MouseMove += MouseClickMove;
+
+            pic2.MouseDown += MouseClickDown;
+            pic2.MouseUp += MouseClickUp;
+            pic2.MouseMove += MouseClickMove;
+            label2.Visible = false;
+            label1.Visible=false;
+            label4.Visible = false;
+            label3.Visible = true;
+            KeyPreview = true;
+
+        }
+        private void MouseClickDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            pos.X = e.X;
+            pos.Y = e.Y;
+        }
+        private void MouseClickUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+
+        }
+        private void MouseClickMove(object sender, MouseEventArgs e)
+        {
+            if(dragging)
+            {
+                Point point=PointToScreen(new Point(e.X,e.Y));
+                this.Location=new Point(point.X-pos.X,point.Y-pos.Y+pic1.Top);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lose) return;
+           
+            int speed = 7;
+            if((e.KeyCode==Keys.Left || e.KeyCode==Keys.A)&&player.Left>95)
+            {
+                player.Left -= speed;
+            }
+            else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && player.Left <355)
+            {
+                player.Left += speed;
+            }
+        }
+       
+
+        private void timer_Tick_1(object sender, EventArgs e)
+        {
+            int speed = 5;
+            pic1.Top += speed;
+            pic2.Top += speed;
+            if (pic1.Top >= 360)
+            {
+                pic1.Top = 0;
+                pic2.Top = -360;
+
+            }
+            coin.Top += speed;
+            if (coin.Top >= 360)
+            {
+                coin.Top = -60;
+                Random rand = new Random();
+                coin.Left = rand.Next(100, 345);
+            }
+            int speedCar = 5;
+
+            bot1.Top +=speedCar;
+            bot2.Top += speedCar;
+            if(bot1.Top>=310)
+            {
+                bot1.Top = -100;
+                Random rand=new Random();
+                bot1.Left = rand.Next(100, 345);
+            }
+
+            if (bot2.Top >= 310)
+            {
+                bot2.Top = -100;
+                Random rand = new Random();
+                bot2.Left = rand.Next(100, 345);
+            }
+            if (player.Bounds.IntersectsWith(bot1.Bounds) || player.Bounds.IntersectsWith(bot2.Bounds) )
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                label4.Text = "Собранные монеты " + countCoins.ToString();
+                label3.Visible = false;
+                label4.Visible = true;
+
+                timer.Enabled = false;
+                lose = true;
+
+            }
+            if(player.Bounds.IntersectsWith(coin.Bounds))
+            {
+                countCoins++;
+                label3.Text="Монеты "+countCoins.ToString();
+                coin.Top = -60;
+                Random rand = new Random();
+                coin.Left = rand.Next(100, 345);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            bot1.Top = -100;
+            bot2.Top = -130;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = true;
+            label4.Visible = false;
+
+            timer.Enabled = true;
+            lose = false;
+            countCoins = 0;
+            label3.Text = "Монеты " + countCoins.ToString();
+            coin.Top = -400;
+
+        }
+    }
+}
